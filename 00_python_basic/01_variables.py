c5140429cf18738e6c8404241d23b47134fd66d2
vivekmittal110 <vivekmittal110230@gmail.com>
2024-01-25 19:51:25 +0530

    update

---
+++ 00_python_basic/01_variables.py
@@ -0,0 +1,15 @@
+x = 10  # int
+y = 3.4  # float
+z = "vivek mittal"   # strings
+a = False  # boolean 
+print(x,y,z,a)
+# Check the data type of variable
+print(type(x))
+
+# Concatination
+print("my name is " + z)
+print("my name is " + z + "my roll no is ", x)
+# we can not concatinate different data types
+
+# Saparator 
+print(x,y,z,a, sep="-")
